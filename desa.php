c96559a8cf00c96325ababf14f891393fc887e0b
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-07-14 14:16:06 +0800

    mengubah sedikit pada desa.php

--- desa.php
+++ desa.php
@@ -7,11 +7,6 @@ session_start();
 if (isset($_SESSION['region'])) {
     $region = $_SESSION['region'];
     $imagePath = "img/$region/desa.png";
-
-    echo "Selamat datang di desa $region!";
-    echo "<img src='$imagePath' alt='$region'>";
-} else {
-    echo "region tidak ditemukan";
 }
 
 
@@ -25,11 +20,13 @@ if (isset($_SESSION['region'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>ini desa</title>
 </head>
 
 <body>
-    <h1>ini desa ...</h1>
+    <h1>Selamat Datang di <?= $region; ?></h1>
+    <img src="<?= $imagePath; ?>" alt="logoDesa">
 </body>
 
 </html>
