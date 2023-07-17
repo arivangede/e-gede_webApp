859c45790de24433d0ed67c084bed8d72ae9aeaa
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-07-17 11:08:53 +0800

    start otw ke homepage desa

--- pilihdesa.php
+++ pilihdesa.php
@@ -1,18 +1,20 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
 </head>
+
 <body>
-   <form action=""method ="post">
+   <form action="" method="post">
 
-<label for="">Pilih Desa</label>
-<input type="text" name="pilih_desa" requir autocomplete="off">
+      <label for="">Pilih Desa</label>
+      <input type="text" name="pilih_desa" requir autocomplete="off">
 
 
-   </form> 
+   </form>
 </body>
+
 </html>
