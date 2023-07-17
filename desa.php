4623571b192afec31b0bb8d91f20232b5719458c
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-07-17 14:12:13 +0800

    memasang carrousel pada homepage

--- desa.php
+++ desa.php
@@ -4,6 +4,9 @@ require 'fungsi/functions.php';
 
 session_start();
 
+$kecamatan = "Denpasar Utara";
+$kota = "Denpasar";
+
 if (isset($_SESSION['region'])) {
     $region = $_SESSION['region'];
     $imagePath = "img/$region/desa.png";
@@ -25,8 +28,27 @@ if (isset($_SESSION['region'])) {
 </head>
 
 <body>
-    <h1>Selamat Datang di <?= $region; ?></h1>
-    <img src="<?= $imagePath; ?>" alt="logoDesa">
+    <header class="head">
+        <div class="logo-nama-desa">
+            <img class="logo-desa" src="<?= $imagePath; ?>" alt="logoDesa">
+            <div class="nama-desa">
+                <h1><?= $region; ?></h1>
+                <p>Kec. <?= $kecamatan; ?> Kota <?= $kota; ?></p>
+            </div>
+        </div>
+    </header>
+
+    <div class="body">
+        <div class="carrousel-card">
+            <section class="card-carrousel 1">
+                <img src="img/assets/asset-2-carrousel-homepage.png" alt="iklan">
+            </section>
+            <section class="card-carrousel 2">
+                <img src="img/assets/asset-3-carrousel-homepage.png" alt="iklan">
+            </section>
+        </div>
+    </div>
+
 </body>
 
 </html>
