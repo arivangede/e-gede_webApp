4f3c9acd351dbe072305ac6fdfbd99cf24f51395
tuedi <iputuedisetiawan8@gmail.com>
2023-07-31 17:10:02 +0800

    e-news dan regulasi(40)

---
+++ fitur/regulasi/produk-hukum.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk Hukum</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../../css/regulasi.css">
+</head>
+<body>
+    <div class="head-produk-hukum">
+        <div class="btnkembali-produk-hukum">
+            <a href=""><i class='bx bx-chevron-left'></i> Kembali</a>
+        </div>
+        <div class="judul-produk-hukum">
+            <div class="pembungkus-judul">
+            <div class="judul1">
+                <h1>Produk Hukum</h1>
+            </div>
+            <div class="judul2">
+                <h2>Dauh Puri Kaja</h2>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ fitur/regulasi/regulasi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Regulasi</title>
+    <link rel="stylesheet" href="../../css/regulasi.css" />
+  </head>
+  <body>
+    <div class="head-regulasi">
+      <div class="head-regulasi-img">
+        <img src="../../img_regulasi/regulasi/Assets 1.png" alt="" />
+      </div>
+      <div class="judul-regulasi">
+      <div class="head-regulasi-tittle">
+        <p>Regulasi Desa</p>
+      </div>
+      <div class="head-regulasi-tittle1">
+        <!------------------- isikan session ---------------->
+        <p>Desa Dauh Puri Kaja, Kec. Denpasar Utara</p>
+      </div>
+      </div>
+    </div>
+    <div class="kategori-regulasi">
+      <p>kategori</p>
+    </div>
+    <div class="body-regulasi">
+      <div class="hukum-regulasi">
+        <img src="../../img_regulasi/regulasi/Assets 4.png" alt="">
+      </div>
+      <div class="public-regulasi">
+        <img src="../../img_regulasi/regulasi/Assets 5.png" alt="">
+      </div>
+    </div>
+  </body>
+</html>
