f2cdf487893deec18fdd7fdebacbb8a3b1e81449
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-08-21 10:22:08 +0800

    menambahkan framework css Tailwind untuk tampilan Panel Admin

---
+++ panel-admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/tailwindcss/tailwind-style.css">
+    <title>Panel Admin | e-GeDe</title>
+</head>
+
+<body>
+    <h1>halo dunia</h1>
+</body>
+
+</html>
